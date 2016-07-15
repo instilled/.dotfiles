63990540bbd01a56d478a222784496fe0b1b1236
Fabio Bernasconi <fabio@contovista.com>
2016-07-15 17:13:08 +0200

    Clean up aliases

--- zsh/source/default/40-aliases.zsh
+++ zsh/source/default/40-aliases.zsh
@@ -1,23 +1,39 @@
 #!/bin/zsh
 
-###>> Aliases
-alias e='emacs -nw'
-alias ec='emacsclient -a emacs --no-wait --socket emacs-server '
-
-alias f='grep -r'
-
-alias diff='grc diff'
-
-# some heavy used change directory aliases
+#--------------------
+# For sane cd
 alias ..='cd ..'
 alias ...='.. && cd ..'
 alias ....='... && cd ..'
 alias h='cd ~/'
 alias dl='cd ~/Downloads'
 
-# Git related aliases
-# See http://nuclearsquid.com/writings/git-tricks-tips-workflows/
+alias l='ls -lh'
+alias ll='ls -lha'
+alias f='grep -r'
+alias diff='grc diff'
+alias mk='mkdir -p'
+alias mc='MC_SKIN="$HOME/.config/mc/nicedark.ini" EDITOR="vim" mc'
+alias p='print -l'
+alias pp='p $path'
+alias ps="ps aux | grep "
+alias sudo='sudo '
+# Assume ~/.ssh/config has a Host definition
+# setting credentials & co
+alias ssht='ssh -f -N '
+alias sk='keybase'
+alias s='gpg'
+#alias secs'keybase pgp'
+alias y='pbcopy <'
 
+#--------------------
+# Emacs
+alias e='emacs -nw'
+alias ec='emacsclient -a emacs --no-wait --socket emacs-server '
+
+#--------------------
+# Git
+# See http://nuclearsquid.com/writings/git-tricks-tips-workflows/
 alias g='git'
 #alias gl='g log --graph --decorate --all --oneline'
 alias gl='g l'
@@ -36,22 +52,19 @@ alias gchb='g checkout -B'
 alias gm='g merge'
 alias gsa='g submodule add'
 
-alias l='ls -lh'
-alias ll='ls -lha'
+#--------------------
+# Docker related aliases
+alias d='docker'
+alias ds='d ps'
+alias dk='d kill'
 
-alias mk='mkdir -p'
-alias mc='MC_SKIN="$HOME/.config/mc/nicedark.ini" EDITOR="vim" mc'
+#--------------------
+# Vagrant
+alias v='vagrant'
+alias vx='vagrant-exec'
 
-alias p='print -l'
-alias pp='p $path'
-alias ps="ps aux | grep "
-
-alias sudo='sudo '
-# Assume ~/.ssh/config has a Host definition
-# setting credentials & co
-alias ssht='ssh -f -N '
-
-# tmux
+#--------------------
+# TMUX
 alias t='tmux '
 alias ts='t new -s '
 alias tl='t list-sessions'
@@ -59,15 +72,3 @@ alias ta='t attach -t '
 alias td='t detach'
 alias tm='t move-window'
 alias tms='tm -t'
-
-alias v='vagrant'
-alias vx='vagrant-exec'
-
-alias sk='keybase'
-alias s='gpg'
-# TODO: fb: add aliases for
-# * sign
-# * encrypt
-#alias secs'keybase pgp'
-
-alias y='pbcopy <'
